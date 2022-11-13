f74a9937b11edc3f85ad659e3385ba1d4a1b8431
tofayel <tajuddinahmed450@gmail.com>
2022-11-13 15:46:57 +0600

    daemon thread and thread sleep

--- JavaMultiThreading/src/main/java/org/example/Main.java
+++ JavaMultiThreading/src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        printing Main thread name
+        System.out.println(Thread.currentThread().getName());
+        MyThread1 myClass = new MyThread1();
+        myClass.start();
+        MyThread myThread = new MyThread();
+        Thread thread = new Thread(myThread);
+        thread.start();
+        Thread thread1 = new Thread(()->{
+            System.out.println("this thread is created with lambda expression");
+        }, "My Thread 1");
+        thread1.start();
+        System.out.println("Name of thread 1 : " + thread1.getName());
+
+//        Daemon thread
+        MyThread1 daemonThread = new MyThread1();
+        daemonThread.setDaemon(true);
+        daemonThread.start();
+        System.out.println("is daemon : " + daemonThread.isDaemon());
+
     }
 }
--- JavaMultiThreading/src/main/java/org/example/MyThread.java
+++ JavaMultiThreading/src/main/java/org/example/MyThread.java
@@ -2,7 +2,15 @@ package org.example;
 
 public class MyThread implements Runnable{
     @Override
-    public void run(){
-        System.out.println("inside MyThread.....");
+    public void run() {
+//        System.out.println("inside MyThread.....");
+        for(int i = 1; i < 11; i++){
+            System.out.println("Value of i : " + i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
