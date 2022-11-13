203c708ba56c16da622906b9118bdec7abf9809a
tofayel <tajuddinahmed450@gmail.com>
2022-11-13 08:35:19 +0600

    creating thread with Runnable interface, thread class, and lambda.

---
+++ JavaMultiThreading/src/main/java/org/example/MyThread.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MyThread implements Runnable{
+    @Override
+    public void run(){
+        System.out.println("inside MyThread.....");
+    }
+}
---
+++ JavaMultiThreading/src/main/java/org/example/MyThread1.java
@@ -0,0 +1,11 @@
+package org.example;
+
+// creating thread by extending Thread class
+public class MyThread1 extends Thread{
+
+@Override
+public void run(){
+    System.out.println("inside MyThread1....");
+}
+
+}
